d9ecb4b75abb5175905ff9c3b288a0234b8bcc91
Max Fischer <maxfischer2781@gmail.com>
2022-08-08 18:44:12 +0200

    removed redundant check

--- uvicorn/server.py
+++ uvicorn/server.py
@@ -316,12 +316,7 @@ class Server:
                 os.kill(os.getpid(), self._delayed_signal)
 
     def install_signal_handlers(self) -> None:
-        if threading.current_thread() is not threading.main_thread():
-            # Signals can only be listened to from the main thread.
-            return
-
         loop = asyncio.get_event_loop()
-
         try:
             for sig in HANDLED_SIGNALS:
                 loop.add_signal_handler(sig, self.handle_exit, sig, None)
