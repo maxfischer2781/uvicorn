a5ecd2ab6ee42b399736558528dcf06f93747725
Max Fischer <maxfischer2781@gmail.com>
2022-08-10 14:18:20 +0200

    added test for signal restoration

---
+++ tests/test_server.py
@@ -0,0 +1,46 @@
+import asyncio
+import os
+import signal
+
+import pytest
+
+from uvicorn.config import Config
+from uvicorn.server import Server
+
+
+class CaughtSigint(Exception):
+    pass
+
+
+@pytest.fixture
+def graceful_sigint():
+    """Fixture that replaces SIGINT handling with a normal exception"""
+    def raise_handler(*args):
+        raise CaughtSigint
+
+    original_handler = signal.signal(signal.SIGINT, raise_handler)
+    yield CaughtSigint
+    signal.signal(signal.SIGINT, original_handler)
+
+
+async def dummy_app(scope, receive, send):
+    pass
+
+
+@pytest.mark.anyio
+async def test_server_interrupt(graceful_sigint):
+    async def interrupt_running(srv: Server):
+        while not srv.started:
+            await asyncio.sleep(0.01)
+        os.kill(os.getpid(), signal.SIGINT)
+
+    server = Server(
+        Config(
+            app=dummy_app,
+            loop="asyncio",
+            limit_max_requests=1,
+        )
+    )
+    asyncio.create_task(interrupt_running(server))
+    with pytest.raises(graceful_sigint):
+        await server.serve()
